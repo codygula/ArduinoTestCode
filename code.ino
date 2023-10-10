8cf16c930dfafca81bf67820e484214c5976c156
C Gula <cgula7@gmail.com>
2023-10-10 10:38:28 -0700

    Working Code

--- code.ino
+++ code.ino
@@ -1,55 +1,39 @@
+#include <ArduinoJson.h>
+#include <Arduino.h> 
+#include <DueTimer.h>  
+#include <XYscope.h>
 
-#define CompileDate __DATE__
-#define CompileTime __TIME__
-char CompileDateStamp[] = CompileDate;//This is how we get a compile date and time stamp into the program
-char CompileTimeStamp[] = CompileTime;
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code Begin -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+#define BTN23_PIN 23
+#define BTN25_PIN 25
+#define BTN27_PIN 27
+#define BTN29_PIN 29
 
-char startMsg[] = "CRT_SCOPE (Ver_1.60) ";	//Program Revision Text
+// DEBOUCE STUFF
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 1000;    // the debounce time; increase if the output flickers
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code End -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 
-
-#include <Arduino.h>	//Provided as part of the Arduino IDE
-
-#include <DueTimer.h>	//Timer library for DUE; download this library from the Arduino.org site
-						//Timer library is also available from author at https://github.com/ivanseidel/DueTimer
-						
-//UNCOMMENT ONE OF THE FOLLOWING TWO INCLUDE STATEMENTS TO SET THE PATH TO THESE THE LIBRARY PROGRAMS
-
-#include <XYscope.h>	//UNCOMMENT THIS LINE for PUBLIC XYscope Drivers & Graphics Functions for Arduino Graphics Engine (AGI)
-						//Use this INCLUDE if you store XYscope library routines inside of ...\Library\XYscope folder
-
-//#include "XYscope.h"	//UNCOMMENT THIS LINE for PRIVATE XYscope Drivers & Graphics Functions for Arduino Graphics Engine (AGI)
-						//Use this INCLUDE if you keep XYscope library routines in same directory as mainline.ino Directory
-
-//Download this library from GitHub
-
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ XYscope code Begin -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 XYscope XYscope;
 
-// 	+---------Begin Critical Interrupt Service Routines ------------+
-//	|  These routines MUST be declared as shown at the top of the	|
-//	|      user's main line code for all XYscope Projects!			|
-// 	+---------------------------------------------------------------+
-//	|																|
-//	V																V
-
 void DACC_Handler(void) {
-	//	DACC_Handler Interrupt Service Routine. This routine
-	//	provides a 'wrapper' to link the AVR DAC INTERRUPT
-	//	to the 'XYscope-class' ISR routine.
-	XYscope.dacHandler();	//Link the AVR DAC ISR/IRQ to the XYscope.
-							//It is called whenever the DMA controller
-							//'DAC_ready_for_More_data' event occurs
+  //	DACC_Handler Interrupt Service Routine. This routine
+  //	provides a 'wrapper' to link the AVR DAC INTERRUPT
+  //	to the 'XYscope-class' ISR routine.
+  XYscope.dacHandler();  //Link the AVR DAC ISR/IRQ to the XYscope.
+                         //It is called whenever the DMA controller
+                         //'DAC_ready_for_More_data' event occurs
 }
 
 void paintCrt_ISR(void) {
-	//	paintCrtISR  Interrupt Service Routine. This routine
-	//	provides a 'wrapper' to link the Timer3.AttachedInterrupt()
-	//	function to the 'XYscope-class' ISR routine.
-	XYscope.initiateDacDma();	//Start the DMA transfer to paint the CRT screen
+  //	paintCrtISR  Interrupt Service Routine. This routine
+  //	provides a 'wrapper' to link the Timer3.AttachedInterrupt()
+  //	function to the 'XYscope-class' ISR routine.
+  XYscope.initiateDacDma();  //Start the DMA transfer to paint the CRT screen
 }
-
-//	V																V
-//	|																|
-// 	+---------- END Critical Interrupt Service Routines ------------+
 
 //	Define/initialize critical global constants and variables
 
@@ -64,108 +48,210 @@ uint32_t dispTimer = 0;
 int MovingX = 2048, MovingY = 2048;
 
 
-int enabSecondHand;	//Flag to turn "Radar Scope clock Second-hand" demo feature on/off
-					//0=Disable second-hand animation, <>0=ENABLE second-hand animation
-
-
 float angle = 0;
 
-#include <malloc.h>	//Required for RAM usage monitoring routines
+#include <malloc.h>  //Required for RAM usage monitoring routines
 
 // The following lines are used for RAM monitoring routines
 extern char _end;
 extern "C" char *sbrk(int i);
-char *ramstart = (char *) 0x20070000;
-char *ramend = (char *) 0x20088000;
+char *ramstart = (char *)0x20070000;
+char *ramend = (char *)0x20088000;
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ XYscope code End -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 
+
+
+
+StaticJsonDocument<500> doc;
+
+const byte numChars = 128;
+char receivedChars[numChars];
+
+boolean newData = false;
 
 void setup() {
-	// Mainline program SETUP routine.
-	//
-	//	Passed Parameters	NONE
-	//
-	//	Returns: NOTHING
-	//
-	//	20170708 Ver 0.0	E.Andrews	First cut
-	//						(Updated throughout development cycle without version change)
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 
-	//Set Serial Monitor must be setup to agree with this initializtion.
-	//  [X] Autoscroll
-	//  "No Line Ending"
-	//  "115200 baud"
-	Serial.begin(115200);
-	
-	//Send startup messages out to Serial monitor port...
-	Serial.println("");
-	Serial.print(startMsg);
-	Serial.print(" (");
-	Serial.print(CompileDateStamp);
-	Serial.print(" ");
-	Serial.print(CompileTimeStamp);
-	Serial.println(")");
+// Button Setup
+pinMode(BTN23_PIN, INPUT); 
+pinMode(BTN25_PIN, INPUT); 
+pinMode(BTN27_PIN, INPUT); 
+pinMode(BTN29_PIN, INPUT); 
 
-	double DmaFreq=800000;		//800000 (Hz) is the default startup value for the DMA frequency.
-								//You can try various alternate values to find an optimal
-								//value that works best for your scope, setup, & application.
-								//Use program menu option "c" & "C" to vary the frequency while
-								//watching your scope display; Using "c" & "C" options will allow you
-								//to see how frequency changes effect the display quality in real-time.
 
-	XYscope.begin(DmaFreq);
 
-	//Timer3 is used as the CRT refresh timer.  This timer is setup inside of XYscope.begin( ).
-	//However, paintCRT_ISR must be "attached" to timer 3.  To be properly link to the
-	//refresh-screen XYscope interupt service routine, it must be linked in the Arduino 
-	//setup() code as follows:
-	
-	Timer3.attachInterrupt(paintCrt_ISR);
 
-	//Here is just some stuff to paint onto CRT at startup
-	//v----------BEGIN SETUP SPLASH SCREEN ---------------v
-	// ArduinoSplash();				//Paint an Arduino logo
-	// int xC = 1800, yC = 2800;		//Set values of XY center coordinates for start of text
-	// int textSize = 400;				//Set Text Size (in pixels)
-	// bool const UndrLined = true;	//Turn underline ON
-	// int textBright = 150;
-	// XYscope.printSetup(xC - 150, yC + 50 + 700, textSize, textBright);
-	// XYscope.print((char *)"AGI", UndrLined);
-	// xC = 100;
-	// yC = 2900;
-	// textSize = 250;
-	// XYscope.printSetup(xC + 50, yC + 50 + textSize, textSize, textBright);
-	// XYscope.setFontSpacing(XYscope.prop);			//Select Proportional Spacing
-	// if (XYscope.getFontSpacing() != XYscope.mono)	//Adjust coordinate in currently active spacing mode is 'prop'
-	// 	XYscope.printSetup(xC + 50 + 500, yC + 50 + textSize, textSize,
-	// 			textBright);
-	// XYscope.print((char *)"Arduino Graphics Interface", false);	//(false=No underline)
-	// XYscope.setFontSpacing(XYscope.mono);
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ XYscope code Begin -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+  double DmaFreq = 800000;  //800000 (Hz) is the default startup value for the DMA frequency.
+                            //You can try various alternate values to find an optimal
+                            //value that works best for your scope, setup, & application.
+                            //Use program menu option "c" & "C" to vary the frequency while
+                            //watching your scope display; Using "c" & "C" options will allow you
+                            //to see how frequency changes effect the display quality in real-time.
 
-	// XYscope.autoSetRefreshTime();
-	// XYscope.plotRectangle(0, 0, 4095, 4095);
+  XYscope.begin(DmaFreq);
 
-	// XYscope.printSetup(350, 275, 175, 100);
-	// XYscope.print(startMsg);XYscope.print((char *)" LibRev:");XYscope.print(XYscope.getLibRev(),2);
+  //Timer3 is used as the CRT refresh timer.  This timer is setup inside of XYscope.begin( ).
+  //However, paintCRT_ISR must be "attached" to timer 3.  To be properly link to the
+  //refresh-screen XYscope interupt service routine, it must be linked in the Arduino
+  //setup() code as follows:
 
-	// //Now put the compile date & time onto the screen
-	// //The time and data stamp come from #define statements at the top of the program...
-	// XYscope.printSetup(1100, 100, 150, 100);
-	// XYscope.print((char *)"(");
-	// XYscope.print(CompileDateStamp);
-	// XYscope.print((char *)"  ");
-	// XYscope.print(CompileTimeStamp);
-	// XYscope.print((char *)")");
+  Timer3.attachInterrupt(paintCrt_ISR);
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ XYscope code End -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 
-	// //^----------BEGIN SETUP SPLASH SCREEN ---------------^
-
-	// //Send option menu out to PC via Serial.print
-	// Serial.println();
-	// Print_CRT_Scope_Menu();
-	// PrintStatsToConsole();
 
 }
 
-void loop() {	
+void loop() {
+  digitalWrite(5, HIGH);
+  recvWithStartEndMarkers();
+  showNewData();
 
-  XYscope.print((char *)"Arduino Graphics Interface", false);
-	Serial.println(" DONE PeakToPeak_Horiz Sq Wave ");
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code Begin -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+
+  if(buttonPressed(BTN23_PIN)) {
+    Serial.print("Button 2 pressed!");
+  }
+  if(buttonPressed(BTN25_PIN)) {
+    Serial.print("Button 3 pressed!");
+  }
+    if(buttonPressed(BTN27_PIN)) {
+    Serial.print("Button 4 pressed!");
+  }
+    if(buttonPressed(BTN29_PIN)) {
+    Serial.print("Button 5 pressed!");
+  }
+  delay(100);
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code End -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 }
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      } else {
+        receivedChars[ndx] = '\0';  // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+void showNewData() {
+  if (newData == true) {
+    Serial.print("(receivedChars = ");
+    delay(500);
+    Serial.println(receivedChars);
+
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, receivedChars);
+
+    // Test if parsing succeeds.
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      digitalWrite(6, HIGH);
+      delay(500);
+      return;
+    }
+
+    const char* display = doc["display"];
+    const char* line1 = doc["line1"];
+    const char* line2 = doc["line2"];
+    const char* line3 = doc["line3"];
+    const char* line4 = doc["line4"];
+
+    Serial.println(line1);
+    Serial.println(line2);
+    Serial.println(line3);
+    Serial.println(line4);
+
+
+    
+    // TEST STRING    <{'display': 'True', 'line1': 'OPTION1', 'line2': 'Option2', 'line3': 'namehereOption3', 'line4': 'namehereOption4'}>
+
+    // If TEST STRING is too long, it will cut off end of srtring and cause "deserializeJson() failed: " error!
+
+    if (strcmp(display,"True")==0){
+      XYscope.plotClear();
+      XYscope.printSetup(700, 700, 200, 100);
+      XYscope.print((char *)line1, false);
+
+      XYscope.printSetup(700, 500, 200, 100);
+      XYscope.print((char *)line2, false);
+
+      XYscope.printSetup(700, 300, 200, 100);
+      XYscope.print((char *)line3, false);
+
+      XYscope.printSetup(700, 100, 200, 100);
+      XYscope.print((char *)line4, false);
+
+      newData = false;
+    }
+    
+    // This is probably not needed. The screen does not need to be cleared by a seperate function before displaying something new b/c XYscope.plotClear() is now run at the beginning of "display == True"
+
+    if (strcmp(display,"False")==0){
+      digitalWrite(7, HIGH);
+      XYscope.plotClear();
+      delay(1000);
+      digitalWrite(7, LOW);
+      newData = false;
+    }
+
+    
+  }
+}
+
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code Begin -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+// Frankenstein debounce function. Not sure how all this works, or enen if it is all needed to work, but it works.
+int buttonPressed(uint8_t button) {
+  static uint16_t lastStates = 0;
+  uint8_t state = digitalRead(button);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (state != ((lastStates >> button) & 1)) {
+      lastStates ^= 1 << button;
+      lastDebounceTime = millis();
+      return state == HIGH;
+    }
+  }
+  return false;
+}
+
+
+// // Working, non-debouced code
+// // Generic function to check if a button is pressed
+// int buttonPressed(uint8_t button) {
+//   static uint16_t lastStates = 0;
+//   uint8_t state = digitalRead(button);
+//   if (state != ((lastStates >> button) & 1)) {
+//     lastStates ^= 1 << button;
+//     return state == HIGH;
+//   }
+//   return false;
+// }
+// -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ Button code End -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+
